898d0a1389c4e852e887b51b723cb8950534fa43
Shima <shimaadnanbasmenj@gmail.com>
2022-11-24 12:51:13 +0000

    B-Write-test & CTDD

--- C-TDD/01-roman-numerals/convert-to-old-roman.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,15 @@
 function convertToOldRoman(n) {
-
+    let romanObj = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1};
+ let roman = '';
+  for (let key in romanObj) {
+   let remain = Math.floor(number / romanObj[key]);
+    if (remain >= 0) {
+      for (let i = 0; i < remain; i++) {
+        roman += key;
+      }
+    }
+    number = number % romanObj[key];
+  }
+  return roman;
 }
-
 module.exports = convertToOldRoman;
